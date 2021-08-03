7f4fbc01ca4678d3e70ee7ed13619ac3ba278973
CoderRCAman <www.amank.o.12345@gmail.com>
2021-08-03 23:51:04 +0530

    v0.0.5

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -4,12 +4,7 @@ import { Redirect,Link} from 'react-router-dom'
 function Header() {
     const [redirect, setRedirect] = useState(false)
     const [store, setStore] = useState('');
-    const grabStoreName = async () => {
-        return await JSON.parse(localStorage.getItem('login-cred'));
-    }
-    useEffect(() => {
-        setStore(grabStoreName().store_name);
-    }, [])
+    
     return (
         <nav>
            {redirect && <Redirect to='/'/> }  
@@ -18,7 +13,7 @@ function Header() {
             </ul>
             <p>Billing System</p>
             <ul className='end-section-nav ' >
-                <li className='hover-btn'> <button>{store}</button> </li>
+                <li className='hover-btn'> <button>{localStorage.getItem('login-cred') &&JSON.parse(localStorage.getItem('login-cred')).store_name }</button> </li>
                 <li className='hover-btn'> <button><Link to='/receipt'>Sale Product</Link></button> </li>
                 <li className='hover-btn'> <button onClick={e => { localStorage.clear('login-cred'); setRedirect(true) }} >Logout</button> </li>
             </ul>
